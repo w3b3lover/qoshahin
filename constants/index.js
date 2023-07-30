2d9bdeb63a9e134e0001af1b2b4cac41e8d0b675
w3b3lover <136494917+w3b3lover@users.noreply.github.com>
2023-07-31 00:19:03 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x9b5f575c82f46971a2c0346f1edba567a0e8a3a3";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
